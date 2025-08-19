3af3f13efc629241108a8801d8f5ec40f854a7ad
AlasamAkkit <alasamakkit@gmail.com>
2025-08-19 16:21:16 +0800

    preview images and desc

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -7,24 +7,28 @@ const projectData = [
     title: "EduCareer",
     description: "EduCareer is a web-based platform designed to support centralized learning and career upskilling.",
     link: "/proj1",
+    image: "/assets/Educareer.png"
   },
   {
     id: 2,
     title: "React Jobs",
     description: "React Jobs is a job application board that allows users to post and apply for jobs.",
     link: "/proj2",
+    image: "/assets/Reactjobs.png"
   },
   {
     id: 3,
     title: "Hospital Management System",
-    description: "Coded with Java, HMS is a comprehensive system for managing hospital operations that leverages the power of object-oriented programming.",
+    description: "Coded with Java, HMS is a Command Line Interface (CLI) application designed using the object-oriented design approach to automate the management of hospital operations.",
     link: "/proj3",
+    image: "/assets/HMS.png"
   },
   {
     id: 4,
     title: "Task Tracker",
     description: "Task Tracker is a simple React application that allows users to manage and track their tasks efficiently",
     link: "/proj4",
+    image: "/assets/Tasktracker.png"
   },
 ];
 
@@ -36,6 +40,11 @@ const Projects = () => {
         {projectData.map((project) => (
           <div key={project.id} className="project-card">
             <h2 className="project-title">{project.title}</h2>
+            <img
+              src={project.image}
+              alt={`${project.title} preview`}
+              className="project-image"
+            />
             <p className="project-description">{project.description}</p>
             <Link to={project.link} className="project-button">
               Find Out More
